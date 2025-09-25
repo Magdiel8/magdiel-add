b6c4cc3117465480d4c39b7f2d55594bb82c62d6
Magdiel <magdielnovoasuarez@gmail.com>
2025-09-25 15:14:23 +0100

    s4

--- scripts/s4.sh
+++ scripts/s4.sh
@@ -2,3 +2,13 @@
 #pedir el nombre del fichero
 read -p "introduce el nombre del fichero: " fic
 #comprobar si existe
+resultado=(sudo find / type f -name "$fic" 2>/dev/null)
+#si existe, mostrar su contenido
+if [ -n $resultado ] ; then
+    echo "el fichero existe: $resultado"
+
+else
+    echo "el fichero no existe"
+    exit 1
+fi
+exit 0
